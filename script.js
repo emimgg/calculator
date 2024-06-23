92b10573e596670fb72f5606e5a1a2ef6a7c641a
emimgg <ultralightened@gmail.com>
2024-06-23 17:08:14 -0400

    Add line to print button input on display

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ let firstNumber = 0;
 let secondNumber = 0;
 let operator = ["+", "-", "*", "/"]; 
 
+const display = document.querySelector(".display");
+const operands = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const btns = document.querySelectorAll(".btn");
+
+for (let item of btns) {
+    item.addEventListener("click", () => display.textContent += item.textContent)
+}
 
 
 
 
-
-
-
-
-
-
+function printToDisplay(selection) {
+    display.textContent += selection.textContent;
+}
 
 function operate(firstNumber, secondNumber, operator) {
     if (operator == "+") add(firstNumber, secondNumber);
