8acf7526b7f53901021a02f9c94f6d1b671fb812
emimgg <ultralightened@gmail.com>
2024-06-23 19:32:13 -0400

    Add function to get numbers and operator

--- script.js
+++ script.js
@@ -1,22 +1,92 @@
 let firstNumber = 0;
 let secondNumber = 0;
-let operator = ["+", "-", "*", "/"]; 
+let operator = ""; 
+let rawDisplay = "";
+let cleanDisplay = [];
 
 const display = document.querySelector(".display");
 const operands = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const btns = document.querySelectorAll(".btn");
 
-for (let item of btns) {
-    item.addEventListener("click", () => display.textContent += item.textContent)
+getNumbers();
+getOperator();
+
+function getNumbers() {
+    for (let operand of operands) {
+        operand.addEventListener("click", () => {
+            printToDisplay(operand);
+            if (operator === ""){
+                firstNumber += operand.textContent;
+            } else {
+                secondNumber += operand.textContent;
+            }
+        });
+    }
 }
-
-
-
-
-function printToDisplay(selection) {
-    display.textContent += selection.textContent;
+function getOperator() {
+    for (let symbol of operators) {
+        symbol.addEventListener("click", () => {
+            display.textContent = symbol.textContent;
+            rawDisplay = symbol.textContent;
+            operator = symbol.textContent;
+        });
+    }
 }
+function printToDisplay(e) {
+    rawDisplay += e.textContent;
+    display.textContent = rawDisplay;
+}
+// function getSecondNumber() {
+//     for (let operand of operands) {
+//         operand.addEventListener("click", ()=> {
+//             rawDisplay = operand.textContent;
+//             display.textContent = rawDisplay;
+//         });
+//     }
+// }
+
+// function getUserEquation() {
+//     if (operator === "") {
+//         getFirstNumber()
+//     }
+//         getOperator();
+//     if (operator !== "") {
+//         getSecondNumber();
+//     }
+//     console.log(firstNumber, secondNumber, operator)
+// }
+
+// getUserEquation();
+
+
+
+
+
+
+
+
+
+
+
+// function printToDisplay() {
+//     for (let item of btns) {
+//         item.addEventListener("click", () => {
+//             rawDisplay.push(item.textContent);
+//             display.textContent += item.textContent;
+//             cleanDisplayValue(rawDisplay);
+//         })
+//     }  
+// }
+// console.log(cleanDisplay);
+// function cleanDisplayValue(array) {
+//     cleanDisplay = array.join("");
+//     return cleanDisplay;
+// }
+
+// function printToDisplay(selection) {
+//     display.textContent += selection.textContent;
+// }
 
 function operate(firstNumber, secondNumber, operator) {
     if (operator == "+") add(firstNumber, secondNumber);
