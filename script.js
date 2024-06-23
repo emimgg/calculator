e6c00d138e2f1189157262dce29218b21834814c
emimgg <ultralightened@gmail.com>
2024-06-23 14:18:42 -0400

    Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = ["+", "-", "*", "/"]; 
+
+
+
+
+
+
+
+
+
+
+
+
+function operate(firstNumber, secondNumber, operator) {
+    if (operator == "+") add(firstNumber, secondNumber);
+    if (operator == "-") substract(firstNumber, secondNumber);
+    if (operator == "*") multiply(firstNumber, secondNumber);
+    if (operator == "/") divide(firstNumber, secondNumber);
+}
+
 function add(a, b) {
     return a + b;
 }
